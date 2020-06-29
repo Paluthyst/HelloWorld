f3999a9187df449c24bbb2a79acd5b851700b94a
Christopher Tackett <hatackett2013@gmail.com>
2020-06-29 18:00:56 -0400

    Initial commit

---
+++ venv/new.py
@@ -0,0 +1,11 @@
+#count = 0
+#for letters in "Hello World":
+#    if letters == "l":
+#        count += 1
+#print(count, "letters found")
+test = "Pythong Programing"
+print("String: ", test)
+first_character = test[2:6]
+print("First Characters:  ", first_character)
+last_character = test[-1:]
+print("Last Character(s): ", last_character)
